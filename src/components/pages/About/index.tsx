9f192b470829392180f421dbb6c1105f2475e8e7
junho2343 <wnsak2343@naver.com>
2022-03-24 16:33:09 +0900

    HITS add

--- src/components/pages/About/index.tsx
+++ src/components/pages/About/index.tsx
@@ -18,6 +18,13 @@ function About() {
 
         <Grid top={60} />
         <Project data={data.project} />
+
+        <a href="https://hits.seeyoufarm.com">
+          <img
+            src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fjunho2343"
+            alt=""
+          />
+        </a>
       </Grid>
       {/* <Title text="PROFILE" adornment="Developer" size="big" />
   <Title text="PROFILE" adornment="Developer" />
