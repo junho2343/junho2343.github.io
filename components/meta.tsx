6ecb2c1ab98e9d971d20ca33dedfe400f626374c
junho2343 <wnsak2343@naver.com>
2022-09-13 13:00:04 +0900

    feat: ga 추가

--- components/meta.tsx
+++ components/meta.tsx
@@ -1,15 +1,17 @@
 import Head from "next/head";
-import { HOME_OG_IMAGE_URL, TITLE_TAG } from "lib/constants";
+import { TITLE_TAG } from "lib/constants";
+import Script from "next/script";
 
 const Meta = () => {
   return (
-    <Head>
-      {/* <link
+    <>
+      <Head>
+        {/* <link
         rel="apple-touch-icon"
         sizes="180x180"
         href="/favicon/apple-touch-icon.png"
       /> */}
-      {/* <link
+        {/* <link
         rel="icon"
         type="image/png"
         sizes="32x32"
@@ -21,22 +23,41 @@ const Meta = () => {
         sizes="16x16"
         href="/favicon/favicon-16x16.png"
       /> */}
-      <link rel="manifest" href="/favicon/site.webmanifest" />
-      {/* <link
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        {/* <link
         rel="mask-icon"
         href="/favicon/safari-pinned-tab.svg"
         color="#000000"
       /> */}
-      <link rel="shortcut icon" href="/favicon/favicon.ico" />
-      <meta name="msapplication-TileColor" content="#000000" />
-      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-      <meta name="theme-color" content="#000" />
-      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      <meta name="description" content={`개발자 ${TITLE_TAG}`} />
-      <meta property="og:image" content="/favicon/social_preview.jpeg" />
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-    </Head>
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta
+          name="msapplication-config"
+          content="/favicon/browserconfig.xml"
+        />
+        <meta name="theme-color" content="#000" />
+        <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+        <meta name="description" content={`개발자 ${TITLE_TAG}`} />
+        <meta property="og:image" content="/favicon/social_preview.jpeg" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+      </Head>
+      {/* <!-- Google tag (gtag.js) --> */}
+
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-TKZLCNQKCR"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-TKZLCNQKCR');
+        `}
+      </Script>
+    </>
   );
 };
 
