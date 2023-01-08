8ab612a5a176e7e9224f3a8167a758d69d53d739
junho2343 <junho234323@gmail.com>
2023-01-09 00:12:21 +0900

    :lipstick: 폰트 변겨엉

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,9 +6,14 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css"
+        />
+        {/* <link
           href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
           rel="stylesheet"
-        />
+        /> */}
       </Head>
       <body>
         <Layout>
