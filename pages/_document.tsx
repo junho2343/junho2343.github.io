65b1ca25a48492e3825ca2451db6a6b1d51a2c0c
junho2343 <wnsak2343@naver.com>
2023-02-21 14:44:58 +0900

    :lipstick: font cnd -> file 방식으로 변경

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,11 +5,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link
+        {/* <link
           rel="stylesheet"
           type="text/css"
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css"
-        />
+        /> */}
         {/* <link
           href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
           rel="stylesheet"
